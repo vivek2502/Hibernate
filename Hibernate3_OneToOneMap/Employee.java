9f835564191f836d8e544de313818552ed9d12b1
Vivek Kumar <42957988+vivek2502@users.noreply.github.com>
2019-12-12 13:09:41 +0530

    Create Employee.java

---
+++ Hibernate3_OneToOneMap/Employee.java
@@ -0,0 +1,26 @@
+package p1;
+
+public class Employee {
+	private int eid;
+	private Address empaddr;
+	private String ename;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public Address getEmpaddr() {
+		return empaddr;
+	}
+	public void setEmpaddr(Address empaddr) {
+		this.empaddr = empaddr;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+}
