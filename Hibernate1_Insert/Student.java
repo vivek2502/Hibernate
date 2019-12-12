1d215f2f9b0269bd8f026f36eeeffc3135692014
Vivek Kumar <42957988+vivek2502@users.noreply.github.com>
2019-12-12 12:57:38 +0530

    Create Student.java

---
+++ Hibernate1_Insert/Student.java
@@ -0,0 +1,20 @@
+package p1;
+
+public class Student {
+	String sname;
+	String sid;
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	
+
+}
