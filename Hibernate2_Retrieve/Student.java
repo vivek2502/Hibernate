429c24fb18e7f501aa2822bbd6303b362a8354d6
Vivek Kumar <42957988+vivek2502@users.noreply.github.com>
2019-12-12 13:04:06 +0530

    Create Student.java

---
+++ Hibernate2_Retrieve/Student.java
@@ -0,0 +1,21 @@
+package p1;
+
+public class Student {
+	private String sid;
+	private String sname;
+	
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
