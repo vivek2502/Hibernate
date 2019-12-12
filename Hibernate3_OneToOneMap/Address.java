3897bacbaf0e475eb27e03fbc667a86a63e65f19
Vivek Kumar <42957988+vivek2502@users.noreply.github.com>
2019-12-12 13:09:13 +0530

    Create Address.java

---
+++ Hibernate3_OneToOneMap/Address.java
@@ -0,0 +1,40 @@
+package p1;
+public class Address {
+	private int addid;
+	private String addrlane;
+	private String city;
+	private String state;
+	private String country;
+	public int getAddid() {
+		return addid;
+	}
+	public void setAddid(int addid) {
+		this.addid = addid;
+	}
+	public String getAddrlane() {
+		return addrlane;
+	}
+	public void setAddrlane(String addrlane) {
+		this.addrlane = addrlane;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+
+}
