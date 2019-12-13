607041f2fd02f9bdd4b119b524daa454e23da7ee
Vivek Kumar <42957988+vivek2502@users.noreply.github.com>
2019-12-13 12:22:03 +0530

    Create Book.java

---
+++ Hibernate4_OneToManyMap./Book.java
@@ -0,0 +1,26 @@
+package p1;
+
+public class Book {
+	private int bid;
+	private String bname;
+	private Publisher pid;
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public Publisher getPid() {
+		return pid;
+	}
+	public void setPid(Publisher pid) {
+		this.pid = pid;
+	}
+	
+}
